59cde0f822229f7143b3d1d12ecd576dab38acfc
EFETR <71470196+ef3tr@users.noreply.github.com>
2026-06-30 18:46:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,22 +29,6 @@ from addons.update import VersionChecker
 from addons.settings import update_config
 from addons.logging import get_logger
 from dotenv import load_dotenv
-from flask import Flask
-from threading import Thread
-
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return "PigPig Bot Çalışıyor!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-keep_alive()
 
 load_dotenv()
 
